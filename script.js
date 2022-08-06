66368ca3d537a11c9c3c5017425006ed66aef84c
Caike192 <71226344+Caike192@users.noreply.github.com>
2022-08-05 23:16:36 -0300

    Update script.js
    PT-BR
    Fiz algumas alterações, como:
    
    ─ Criar função mostrar_texto;
    
    ─ Na função encriptar:
     · Criar um objeto que tem as letras no caso vogais como chave e as substituições (palavras aleatórias) como valores, de nome "encriptador";
     · Trocar for(i++) por for..of que lê caractere por caractere da string sem a necessidade de usar endereço do array;
     · (linha: 24-29) Dentro do if com regex coloquei um return vazio, para caso algum valor não desejado entre no input, a função termine imediatamente sem que tenha que fazer outras operações;
     · Criei uma variável chamada "é_vogal";
     · Coloquei um if que verifica se a variável é vogal. Se a letra for vogal, a variável "encriptada" é concatenada com o valor de "encriptador[letra]", exemplo: encriptador["a"] => "ai". Se não, "encriptada" é concatenada com  "letra", exemplo: encriptada = encriptada + "i";
     · E a função mostrar_texto que abstrai os processos que mostram o texto, para que a função possa ser utilizada em "desencriptar" também sem a necessidade de copiar varias linhas de código;
    
    ─ Na função desencriptar:
     · criei outro objeto do mesmo tipo do objeto "encriptador" da função "encriptar", só que as chaves sendo as letras vogais, de nome "desencriptador";
     · substitui os vários replaceAll por um replaceAll, utilizando for..of com "Object.entries(desencriptador)";

--- script.js
+++ script.js
@@ -1,70 +1,74 @@
+function mostrar_texto(texto) {
+    document.getElementById("imgDer").style.display = "none"
+    document.getElementById("texto").style.display = "none"
+    document.getElementById("copiar").style.display = "show"
+    document.getElementById("texto2").innerHTML = texto
+    document.getElementById("textoingresado").innerHTML = " "
+    document.getElementById("copiar").style.display = "inherit"
+}
+
 function encriptar() {
-    var valor = 0;
-    encriptado = ""
-    let textoingresado = document.getElementById("textoingresado").value;
-    let str = textoingresado
-    let arr = Array.from(str);
-    for (i = 0; i < str.length; i++) {
-        if (arr[i].search(/[^A-Z]+/) === -1 | arr[i].search(/[^0-9]+/) === -1 | arr[i].search(/[^!@#$%^&*.,:?¡¿/]+/) === -1 ) {
-            alert("Ingrese solo minusculas; no se puede ingresar numeros o signos ");
-            var contenido1 = document.getElementById("textoingresado");
-            contenido1.value = "";
-            valor = 1
-            break;
+    const encriptadores = {
+        "a": "ai",
+        "e": "enter",
+        "i": "imes",
+        "o": "ober",
+        "u": "ufat"
+    }
+
+    let encriptado = ""
+    const textoingresado = document.getElementById("textoingresado").value
+    const arr = Array.from(textoingresado)
+
+    for (let letra of arr) {
+        if (letra.search(/[^A-Z]+/) === -1 | letra.search(/[^0-9]+/) === -1 | letra.search(/[^!@#$%^&*.,:?¡¿/]+/) === -1 ) {
+            alert("Ingrese solo minusculas; no se puede ingresar numeros o signos ")
+            const contenido1 = document.getElementById("textoingresado")
+            contenido1.value = ""
+            return
         }
         
-        if (arr[i] == "a") {
-            encriptado = encriptado + "ai"
-        }
-        else if (arr[i] == "e") {
-            encriptado = encriptado + "enter"
-        }
-        else if (arr[i] == "o") {
-            encriptado = encriptado + "ober"
-        }
-        else if (arr[i] == "u") {
-            encriptado = encriptado + "ufat"
-        }
-        else if (arr[i] == "i") {
-            encriptado = encriptado + "imes"
+        const é_vogal = letra.search(/[aeiou]/) !== -1
+
+        if (é_vogal) {
+            //encriptado = encriptado + encriptadores[letras]
+            encriptado += encriptadores[letra]
         }
         else {
-            encriptado = encriptado + arr[i]
+            encriptado += letra
         }
     }
-    if (valor == 0) {
-        var contenido = document.getElementById("texto2");
-        contenido.value = encriptado;
-    }
-    document.getElementById("imgDer").style.display = "none";
-    document.getElementById("texto").style.display = "none";
-    document.getElementById("copiar").style.display = "show";
-    document.getElementById("texto2").innerHTML = encriptado;
-    document.getElementById("textoingresado").innerHTML = " ";
-    document.getElementById("copiar").style.display = "inherit";
+
+    mostrar_texto(encriptado)
 }
 
 function desencriptar() {
-    let textoingresado = document.getElementById("textoingresado").value;
-    console.log(textoingresado);
-    arr = textoingresado.replaceAll("ai", "a")
-    arr = arr.replaceAll("enter", "e")
-    arr = arr.replaceAll("imes", "i")
-    arr = arr.replaceAll("ufat", "u")
-    arr = arr.replaceAll("ober", "o")
-    var contenido = document.getElementById("texto2");
-    contenido.value = arr;
-    document.getElementById("imgDer").style.display = "none";
-    document.getElementById("texto").style.display = "none";
-    document.getElementById("copiar").style.display = "show";
-    document.getElementById("texto2").innerHTML = encriptado;
-    document.getElementById("textoingresado").innerHTML = " ";
-    document.getElementById("copiar").style.display = "inherit";
-}
-function copiar() {
-    var contenido = document.querySelector("#texto2");
-    contenido.select();
-    document.execCommand('copy');
-    alert("Texto copiado en el portapapeles");
-  }
+    const descriptadores = {
+        "ai": "a",
+        "enter": "e",
+        "imes": "i",
+        "ufat": "u",
+        "ober": "o"
+    }
 
+    const textoingresado = document.getElementById("textoingresado").value
+
+    console.log(textoingresado)
+
+    let desencriptado = textoingresado
+
+    for (let [cript, letra] of Object.entries(descriptadores)) {
+        desencriptado = desencriptado.replaceAll(cript, letra)
+    }
+    
+    mostrar_texto(desencriptado)
+}
+
+function copiar() {
+    const contenido = document.querySelector("#texto2")
+    contenido.select()
+
+    document.execCommand('copy')
+
+    alert("Texto copiado en el portapapeles")
+}
